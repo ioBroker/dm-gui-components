90200b102187dd519cf86cdc3f09bc4c02532580
GermanBluefox <dogafox@gmail.com>
2026-03-27 16:17:18 +0100

    chore: release v9.0.9
    - (@GermanBluefox) Layout improved

--- build/InstanceActionButton.js
+++ build/InstanceActionButton.js
@@ -4,7 +4,7 @@ import { getTranslation, renderActionIcon } from './Utils';
 export default function InstanceActionButton(params) {
     const { action, instanceHandler } = params;
     const tooltip = getTranslation(action?.description ? action.description : '');
-    const title = getTranslation(action?.title ? action.title : '');
+    const title = getTranslation(action?.title || '');
     const icon = renderActionIcon(action);
     return (React.createElement(TooltipButton, { tooltip: tooltip, label: title, disabled: action.disabled, Icon: icon, onClick: instanceHandler(action) }));
 }
--- build/TooltipButton.js
+++ build/TooltipButton.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import { IconButton, Tooltip, Typography } from '@mui/material';
+import { Button, IconButton, Tooltip, Typography } from '@mui/material';
 export default function TooltipButton(props) {
     const { tooltip, label, disabled, Icon, onClick, url } = props;
-    const text = !!label && (React.createElement(Typography, { variant: "button", style: { marginLeft: 4 } }, label));
+    const text = label ? (React.createElement(Typography, { variant: "button", style: { marginLeft: 4 } }, label)) : null;
     const btnProps = url ? { href: url, disabled, target: '_blank' } : { onClick, disabled };
     if (tooltip) {
         return (React.createElement(Tooltip, { title: tooltip, slotProps: { popper: { sx: { pointerEvents: 'none' } } } },
-            React.createElement("span", null,
-                React.createElement(IconButton, { ...btnProps, size: "small" },
-                    Icon,
-                    text))));
+            React.createElement("span", null, text ? (React.createElement(Button, { ...btnProps, startIcon: Icon }, text)) : (React.createElement(IconButton, { ...btnProps, size: "small" }, Icon)))));
     }
-    return (React.createElement(IconButton, { ...btnProps, size: "small" },
-        Icon,
-        text));
+    return text ? (React.createElement(Button, { ...btnProps, startIcon: Icon }, text)) : (React.createElement(IconButton, { ...btnProps, size: "small" }, Icon));
 }
 //# sourceMappingURL=TooltipButton.js.map
