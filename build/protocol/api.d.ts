fc33c3d69935ee0c1137f3e0624ff4dfa70860dc
Samuel Weibel <samuel.weibel@gmail.com>
2026-03-31 10:28:29 +0000

    Use new "api" export from dm-utils

--- build/protocol/api.d.ts
+++ build/protocol/api.d.ts
@@ -1,2 +1,13 @@
-export type * from '@iobroker/dm-utils/build/types/api';
+import type { api } from '@iobroker/dm-utils';
 export type { ActionButton, ConfigConnectionType, ControlBase, ControlState, DeviceDetails, DeviceId, DeviceStatus, ErrorResponse, JsonFormSchema, ProgressUpdate, } from '@iobroker/dm-utils';
+export type ActionBase = api.ActionBase;
+export type CommunicationForm = api.CommunicationForm;
+export type DeviceAction = api.DeviceAction;
+export type DeviceControl = api.DeviceControl;
+export type DeviceInfo = api.DeviceInfo;
+export type DeviceLoadIncrement = api.DeviceLoadIncrement;
+export type DmActionResponse = api.DmActionResponse;
+export type DmControlResponse = api.DmControlResponse;
+export type InstanceAction = api.InstanceAction;
+export type InstanceDetails = api.InstanceDetails;
+export type InstanceRefreshResponse = api.InstanceRefreshResponse;
--- src/protocol/api.ts
+++ src/protocol/api.ts
@@ -1,4 +1,4 @@
-export type * from '@iobroker/dm-utils/build/types/api';
+import type { api } from '@iobroker/dm-utils';
 
 export type {
     ActionButton,
@@ -12,3 +12,15 @@ export type {
     JsonFormSchema,
     ProgressUpdate,
 } from '@iobroker/dm-utils';
+
+export type ActionBase = api.ActionBase;
+export type CommunicationForm = api.CommunicationForm;
+export type DeviceAction = api.DeviceAction;
+export type DeviceControl = api.DeviceControl;
+export type DeviceInfo = api.DeviceInfo;
+export type DeviceLoadIncrement = api.DeviceLoadIncrement;
+export type DmActionResponse = api.DmActionResponse;
+export type DmControlResponse = api.DmControlResponse;
+export type InstanceAction = api.InstanceAction;
+export type InstanceDetails = api.InstanceDetails;
+export type InstanceRefreshResponse = api.InstanceRefreshResponse;
